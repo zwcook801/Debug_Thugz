923c50d173080d1efba66d21ad1f098e0a624174
Zach Cook <zach.w.cook@gmail.com>
2023-04-09 14:09:03 -0600

    added trailer link to watchlist

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-
 // function getIdGenre() {
 
 //     var requestOptions = {
@@ -10,7 +9,7 @@
 //         .then(response => response.json())
 //         // adds to page instead of console
 //         .then(result => {
-//             // each genre has a button. 
+//             // each genre has a button.
 //             // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 //             result.genres.forEach(element => document.body.innerHTML = document.body.innerHTML + `<button class="genres" genre-id="${element.id}">${element.name}</button>`);
 //             var allGenres = Array.from(document.getElementsByClassName(`genres`));
@@ -24,17 +23,19 @@
 //         .catch(error => console.log('error', error));
 // }
 
-
 function getDetails() {
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
 
-    fetch("https://api.themoviedb.org/3/movie/550?api_key=798d3829156f2a1840e8049c3a0c46b1&language=en-US", requestOptions)
-        .then(response => response.json())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+  fetch(
+    "https://api.themoviedb.org/3/movie/550?api_key=798d3829156f2a1840e8049c3a0c46b1&language=en-US",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
 }
 
 var watchlist = [];
@@ -61,111 +62,133 @@ var watchlist = [];
 // }
 
 function getMovies(genreId) {
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
 
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=798d3829156f2a1840e8049c3a0c46b1&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genreId}&with_watch_monetization_types=flatrate`, requestOptions)
-        .then(response => response.json())
-        .then(result => {
-            console.log("getMovies", result)
-            var moviedivparent = document.getElementsByClassName(`moviesDiv`)[0]
-            result.results.forEach(movie => {
-                var movieDiv = document.createElement('div');
-                movieDiv.className = 'movies';
-                movieDiv.setAttribute('movie-id', movie.id);
-                movieDiv.innerHTML = `
+  fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=798d3829156f2a1840e8049c3a0c46b1&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genreId}&with_watch_monetization_types=flatrate`,
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      console.log("getMovies", result);
+      var moviedivparent = document.getElementsByClassName(`moviesDiv`)[0];
+      result.results.forEach((movie) => {
+        var movieDiv = document.createElement("div");
+        movieDiv.className = "movies";
+        movieDiv.setAttribute("movie-id", movie.id);
+        movieDiv.innerHTML = `
                   <h2 class="movieName">${movie.title}</h2>
-                  <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="movie image"> 
+                  <img src="https://image.tmdb.org/t/p/w500${
+                    movie.poster_path
+                  }" alt="movie image"> 
                   <p class="movieRev">${movie.overview}</p>
                   <p>Rating: ${movie.vote_average}</p>
                   <p>Release Date: ${movie.release_date}</p>
                   <p id=${movie.id + "-link"}>link</p>
-                  <button class="add-to-watchlist" data-movie='${JSON.stringify(movie)}'>Add to Watchlist</button>`;
-                moviedivparent.appendChild(movieDiv);
-            });
-            getNetflixLinks(result.results)
+                  <button class="add-to-watchlist" data-movie='${JSON.stringify(
+                    movie
+                  )}'>Add to Watchlist</button>`;
+        moviedivparent.appendChild(movieDiv);
+      });
 
-
-
-            var addToWatchlistButtons = Array.from(document.getElementsByClassName('add-to-watchlist'));
-            addToWatchlistButtons.forEach(button => {
-                button.addEventListener('click', function (event) {
-                    if (watchlist.length < 4) {
-                        var movieData = JSON.parse(event.target.dataset.movie);
-                        if (!watchlist.some(movie => movie.id === movieData.id)) {
-                            watchlist.push(movieData);
-                            localStorage.setItem('watchlist', JSON.stringify(watchlist));
-                            showWatchlist();
-                            button.innerHTML = "Added to Watchlist"
-                            console.log(watchlist);
-                        } else {
-                            alert("This movie is already in your watchlist!");
-                        }
-                    } else {
-                        alert("You can only add up to 4 movies to your watchlist!");
-                    }
-                });
-            });
-        })
-        .catch(error => console.log('error', error));
+      var addToWatchlistButtons = Array.from(
+        document.getElementsByClassName("add-to-watchlist")
+      );
+      addToWatchlistButtons.forEach((button) => {
+        button.addEventListener("click", async function (event) {
+          if (watchlist.length < 4) {
+            var movieData = JSON.parse(event.target.dataset.movie);
+            if (!watchlist.some((movie) => movie.id === movieData.id)) {
+              const link = await getTrailerLink(movieData.title);
+              console.log("linkretrieve", link);
+              movieData.link = link;
+              watchlist.push(movieData);
+              localStorage.setItem("watchlist", JSON.stringify(watchlist));
+              showWatchlist();
+              button.innerHTML = "Added to Watchlist";
+              console.log(watchlist);
+            } else {
+              alert("This movie is already in your watchlist!");
+            }
+          } else {
+            alert("You can only add up to 4 movies to your watchlist!");
+          }
+        });
+      });
+    })
+    .catch((error) => console.log("error", error));
 }
 
-function getNetflixLinks(movies){
-    console.log("getNetflixLinks", movies)
-    var requestOptions = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'fd4fb301f1mshc9090a171201103p1c9e2ejsn5169072e8ce4',
-            //'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-        }
-    };
-fetch(`https://streaming-availability.p.rapidapi.com/v2/search/title?title=${movies[0].title}&country=us&show_type=movie&output_language=en`, requestOptions)
-.then(response => response.json())
-.then(result => console.log("getlinksresults", result))
+async function getTrailerLink(title) {
+  console.log("getTrailerLink", title);
+  var requestOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "fd4fb301f1mshc9090a171201103p1c9e2ejsn5169072e8ce4",
+      //'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+    },
+  };
+  const data = await fetch(
+    `https://streaming-availability.p.rapidapi.com/v2/search/title?title=${title}&country=us&show_type=movie&output_language=en`,
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((data) => data);
+  console.log("fetchresult", data.result);
+  return data.result[0].youtubeTrailerVideoLink;
 }
-
 
 function getTvshows() {
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
 
-    fetch("https://api.themoviedb.org/3/discover/tv?api_key=798d3829156f2a1840e8049c3a0c46b1&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&with_genres=35&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0", requestOptions)
-        .then(response => response.json())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+  fetch(
+    "https://api.themoviedb.org/3/discover/tv?api_key=798d3829156f2a1840e8049c3a0c46b1&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&with_genres=35&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
 }
 
 function showWatchlist() {
-    var watchlistDiv = document.getElementById('watchlist');
-    watchlistDiv.innerHTML = '';
-    watchlist.forEach(movie => {
-        var movieDiv = document.createElement('div');
-        movieDiv.className = 'movies';
-        movieDiv.setAttribute('movie-id', movie.id);
-        movieDiv.innerHTML = `
+  var watchlistDiv = document.getElementById("watchlist");
+  watchlistDiv.innerHTML = "";
+  watchlist.forEach((movie) => {
+    var movieDiv = document.createElement("div");
+    movieDiv.className = "movies";
+    movieDiv.setAttribute("movie-id", movie.id);
+    movieDiv.innerHTML = `
           <h2>${movie.title}</h2>
+          <div>
+            <a href="${movie.link}">${movie.link}</a>
+          </div>
           <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="movie image">`;
-        watchlistDiv.appendChild(movieDiv);
-        console.log(movie);
-        watchlistDiv.addEventListener('click', function (event) {
-            if (event.target.classList.contains('remove-from-watchlist')) {
-                var movieId = event.target.parentNode.getAttribute('movie-id');
-                watchlist = watchlist.filter(movie => movie.id != movieId);
-                localStorage.setItem('watchlist', JSON.stringify(watchlist));
-                showWatchlist();
-            }
-        });
+    watchlistDiv.appendChild(movieDiv);
+    console.log(movie);
+    watchlistDiv.addEventListener("click", function (event) {
+      if (event.target.classList.contains("remove-from-watchlist")) {
+        var movieId = event.target.parentNode.getAttribute("movie-id");
+        watchlist = watchlist.filter((movie) => movie.id != movieId);
+        localStorage.setItem("watchlist", JSON.stringify(watchlist));
+        showWatchlist();
+      }
+    });
 
-        movieDiv.innerHTML = `
+    movieDiv.innerHTML = `
     <h2>${movie.title}</h2>
+    <div>
+        <a href="${movie.link}">${movie.link}</a>
+    </div>
     <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="movie image">
     <button class="remove-from-watchlist">Remove</button>
 `;
-    });
+  });
 }
 
 // var elem = document.getElementById("genresbtn");
@@ -173,39 +196,46 @@ function showWatchlist() {
 //     getIdGenre();
 // });
 
-var storedWatchlist = localStorage.getItem('watchlist');
+var storedWatchlist = localStorage.getItem("watchlist");
 if (storedWatchlist) {
-    watchlist = JSON.parse(storedWatchlist);
+  watchlist = JSON.parse(storedWatchlist);
 }
 
 // window.addEventListener("load", (event) => {
 //     showWatchlist();
 // });
 
-
 var watchlist = [];
 
 function getIdGenre() {
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
 
-    fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=798d3829156f2a1840e8049c3a0c46b1&language=en-US", requestOptions)
-        .then(response => response.json())
-        .then(result => {
-            console.log("getIdGenre", result)
-            result.genres.forEach(element => document.body.innerHTML = document.body.innerHTML + `<button class="genres" genre-id="${element.id}">${element.name}</button>`);
-            var allGenres = Array.from(document.getElementsByClassName(`genres`));
-            allGenres.forEach(genrebtn => genrebtn.addEventListener('click', function (event) {
-                var genreId = event.target.getAttribute('genre-id')
-                getMovies(genreId)
-            }))
-
+  fetch(
+    "https://api.themoviedb.org/3/genre/movie/list?api_key=798d3829156f2a1840e8049c3a0c46b1&language=en-US",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      console.log("getIdGenre", result);
+      result.genres.forEach(
+        (element) =>
+          (document.body.innerHTML =
+            document.body.innerHTML +
+            `<button class="genres" genre-id="${element.id}">${element.name}</button>`)
+      );
+      var allGenres = Array.from(document.getElementsByClassName(`genres`));
+      allGenres.forEach((genrebtn) =>
+        genrebtn.addEventListener("click", function (event) {
+          var genreId = event.target.getAttribute("genre-id");
+          getMovies(genreId);
         })
-        .catch(error => console.log('error', error));
+      );
+    })
+    .catch((error) => console.log("error", error));
 }
-
 
 // function showWatchlist() {
 //     var watchlistDiv = document.getElementById('watchlist');
@@ -237,16 +267,16 @@ function getIdGenre() {
 // }
 
 var elem = document.getElementById("genresbtn");
-elem.addEventListener('click', function () {
-    getIdGenre();
+elem.addEventListener("click", function () {
+  getIdGenre();
 });
 
-var storedWatchlist = localStorage.getItem('watchlist');
+var storedWatchlist = localStorage.getItem("watchlist");
 if (storedWatchlist) {
-    watchlist = JSON.parse(storedWatchlist);
+  watchlist = JSON.parse(storedWatchlist);
 }
 
 window.addEventListener("load", (event) => {
-    showWatchlist();
-    console.log("windowload")
+  showWatchlist();
+  console.log("windowload");
 });
